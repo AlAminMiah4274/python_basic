fecbea9ed1d2311a66b6f751d51099e6a3f7fdb8
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-11-22 17:06:55 +0600

    known the comparison operator

---
+++ conditional_logic.py
@@ -0,0 +1,33 @@
+num_equal = 2 == 3;
+# print(num_equal);
+
+num_equal2 = 3 == 3;
+# print(num_equal2);
+
+num_less = 2 > 3;
+# print(num_less);
+
+num_greater = 2 < 3;
+# print(num_greater);
+
+num_not_equal = 2 != 3;
+# print(num_not_equal);
+
+num_not_equal2 = 3 != 3;
+# print(num_not_equal2);
+
+num_less_equal = 2 >= 3;
+# print(num_less_equal);
+
+num_greater_equal = 2 <= 3;
+# print(num_greater_equal);
+
+"""comparison operator in Python:
+1. Equal: ==
+2. Not equal: != 
+3. Less than: < 
+4. Greater than: > 
+5. Lessthan or equal: <= 
+6. Greaterthan or equal: >= 
+"""
+
--- excercise.py
+++ excercise.py
@@ -1,38 +1,38 @@
 # using with (-).....
 
-# num_multip1 = input("Please type an integer and press enter: ");
-# num_multip2 = input("Please type another integer and press enter: ");
-# num_multip1 = int(num_multip1);
-# num_multip2 = int(num_multip2);
-# # print("number_1 - number_2 =", num_multip1 - num_multip2);
+num_multip1 = input("Please type an integer and press enter: ");
+num_multip2 = input("Please type another integer and press enter: ");
+num_multip1 = int(num_multip1);
+num_multip2 = int(num_multip2);
+print("number_1 - number_2 =", num_multip1 - num_multip2);
 
-# # using with (*).....
+# using with (*).....
 
-# num_subs1 = input("Please type an integer and press enter: ");
-# num_subs2 = input("Please type another integer and press enter: ");
-# num_subs1 = int(num_subs1);
-# num_subs2 = int(num_subs2);
-# # print("number_1 * number_2 =", num_subs1 * num_subs2);
+num_subs1 = input("Please type an integer and press enter: ");
+num_subs2 = input("Please type another integer and press enter: ");
+num_subs1 = int(num_subs1);
+num_subs2 = int(num_subs2);
+print("number_1 * number_2 =", num_subs1 * num_subs2);
 
-# # using with (/).....
+# using with (/).....
 
-# num_div1 = input("Please type an interger and press enter: ");
-# num_div2 = input("Please type another integer and press enter: ");
-# num_div1 = int(num_div1);
-# num_div2 = int(num_div2);
-# # print("number_1 / number_2 =", num_div1 / num_div2);
+num_div1 = input("Please type an interger and press enter: ");
+num_div2 = input("Please type another integer and press enter: ");
+num_div1 = int(num_div1);
+num_div2 = int(num_div2);
+print("number_1 / number_2 =", num_div1 / num_div2);
 
-# # without float.....
-# num_div3 = input("Please type an integer and press enter: ");
-# num_div4 = input("Please type another integer and press enter: ");
-# num_div3 = int(num_div3);
-# num_div4 = int(num_div4);
-# # print("number_1 / number_2 =", num_div3 // num_div4);
+# without float.....
+num_div3 = input("Please type an integer and press enter: ");
+num_div4 = input("Please type another integer and press enter: ");
+num_div3 = int(num_div3);
+num_div4 = int(num_div4);
+print("number_1 / number_2 =", num_div3 // num_div4);
 
-# # to convert into sqare 
-# num_sqr = input('Please type an integer and press enter: ');
-# num_sqr = int(num_sqr);
-# print('Number_1 ** Number_2 =', num_sqr ** 2);
+# to convert into sqare 
+num_sqr = input('Please type an integer and press enter: ');
+num_sqr = int(num_sqr);
+print('Number_1 ** Number_2 =', num_sqr ** 2);
 
 # modulas - %
 num_modulas1 = input('Please type an integer and press enter: ');
