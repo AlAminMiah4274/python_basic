56dd3fcd0ae915820dd81b21ca66f36e6bf3a0b7
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-11-22 19:05:51 +0600

    learnt and, or, not and list

--- conditional_logic.py
+++ conditional_logic.py
@@ -47,3 +47,55 @@ my_money2 = 60;
 rickshaw_fare2 = 40;
 ride_at_rickshaw2 = my_money2 >= rickshaw_fare2;
 # print(ride_at_rickshaw2);
+
+day = 'Sunday';
+day_true = day == 'Sunday';
+# print(day_true);
+
+day = 'Friday';
+day_false = day == 'Saterday';
+# print(day_false);
+
+today = 'Wednesday';
+day_false = not (today == 'Wednesday');
+# print(day_false);
+
+value_false = not True; 
+# print(value_false);
+
+value_true = not False;
+# print(value_true);
+
+num = 20;
+value_true = num > 10;
+value_false = num > 100;
+
+validation_and = value_true and value_false;
+# print(validation_and);
+
+validation_or = value_true or value_false;
+# print(validation_or);
+
+validation_not = not (value_true == value_false);
+# print(validation_not);
+
+numbers = [1,2,3,57,54, 78, 50, 74];
+# print(numbers);
+total_index = len(numbers);
+# print(index);
+specific_element = numbers[2];
+# print(specific_element);
+
+saarc = ['Bangladesh', 'Afganistan', 'India', 'Bhutan', 'Nepal', 'Pakistan', 'Sri Lanka'];
+# print(saarc);
+# print(saarc[0]);
+# print('Number of countries in SAARC: ', len(saarc));
+
+saarc_element = 'Bangladesh' in saarc;
+# print(saarc_element);
+
+saarc_element = 'Japan' in saarc;
+# print(saarc_element);
+
+saarc_element = 'China' not in saarc;
+# print(saarc_element);
