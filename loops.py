19f21e23e7b56af8ca2cb05fa09fee649e59166a
Al Amin Miah <alaminmiah4274@gmail.com>
2023-11-26 21:24:22 +0600

    drawn a square using turtle and for loop

---
+++ loops.py
@@ -0,0 +1,25 @@
+# for i in range(10):
+    # print('I want to be a great programmer.');
+
+
+# for i in range(5):
+#     print('I am the best.')
+
+# for i in range(9):
+#     print('Yes, I can controll everything');
+
+# n = 15;
+
+# for i in range(5):
+#     print(i);
+
+import turtle
+
+turtle.shape('turtle');
+turtle.speed(5);
+
+for i in range(4):
+    turtle.forward(100);
+    turtle.left(90);
+
+turtle.exitonclick();
