20e3142f3e4587d98bd69790dff07041c0547c47
Al Amin Miah <alaminmiah4274@gmail.com>
2023-11-26 20:59:21 +0600

    has been done a program using turtle

---
+++ graphics.py
@@ -0,0 +1,26 @@
+import turtle
+# turtle.forward(100)
+# turtle.exitonclick()  # exitonclick() a function or method is used to exit by clicking
+
+# DRAWN A SQUARE:
+# turtle.shape('turtle');
+
+# turtle.forward(100);
+# turtle.left(90);
+# turtle.forward(100);
+# turtle.left(90);
+# turtle.forward(100);
+# turtle.left(90);
+# turtle.forward(100);
+
+# turtle.exitonclick();
+
+# DRAWN A TRIANGLE:
+turtle.shape('turtle');
+turtle.speed(8);
+
+turtle.forward(130);
+turtle.left(130);
+turtle.forward(130);
+
+turtle.exitonclick();
