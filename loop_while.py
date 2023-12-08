c039d8630e852b5b59a9e586e6811d458eb53c55
Al Amin Miah <alaminmiah4274@gmail.com>
2023-12-08 15:27:52 +0600

    drawn a dot square with python turtle

--- loop_while.py
+++ loop_while.py
@@ -61,3 +61,24 @@
 #     turtle.left(120);
 
 # turtle.exitonclick();
+
+import turtle;
+
+height = 5;
+width = 8;
+
+turtle.speed(2);
+turtle.shape("turtle");
+turtle.color("purple");
+turtle.penup();
+
+for y in range(height):
+    for x in range(width):
+        turtle.dot();
+        turtle.forward(20);
+    turtle.backward(20 * width);
+    turtle.right(90);
+    turtle.forward(20);
+    turtle.left(90);
+
+turtle.exitonclick();
