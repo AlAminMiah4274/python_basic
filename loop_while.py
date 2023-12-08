603c138b210a55d31416728eb3e77ff60f03af12
Al Amin Miah <alaminmiah4274@gmail.com>
2023-12-08 15:06:45 +0600

    drawn a triangle with python turtle

--- loop_while.py
+++ loop_while.py
@@ -1,71 +1,63 @@
-i = 0
-while i < 5:
-    print(i)
-    i += 1
+# i = 0
+# while i < 5:
+#     print(i)
+#     i += 1
 
-i = 5
-while i > 1:
-    i -= 1
-    print(i)
+# i = 5
+# while i > 1:
+#     i -= 1
+#     print(i)
 
-multiplication_table/Namta
+# # multiplication_table/Namta
 
-n = 9
-m = 1
-while m <= 10:
-    print(n, "X", m, "=", n*m)
-    m += 1
+# n = 9
+# m = 1
+# while m <= 10:
+#     print(n, "X", m, "=", n*m)
+#     m += 1
 
-n = input("Please type a postive integer number and press enter: ")
-n = int(n)
-for i in range(1, 11):
-    print(n, "X", i, "=", n*i)
-    i += 1
+# n = input("Please type a postive integer number and press enter: ")
+# n = int(n)
+# for i in range(1, 11):
+#     print(n, "X", i, "=", n*i)
+#     i += 1
 
-n = 15
-for i in range(1, 11):
-    print(n, "X", i, "=", n*i)
-    i += 1
+# n = 15
+# for i in range(1, 11):
+#     print(n, "X", i, "=", n*i)
+#     i += 1
 
-n = input("Please write a positive integer number and press enter: ")
-n = int(n)
+# n = input("Please write a positive integer number and press enter: ")
+# n = int(n)
 
-m = 1
-while m <= 10:
-    print(n, "X", m, "=", n*m)
-    m += 1
+# m = 1
+# while m <= 10:
+#     print(n, "X", m, "=", n*m)
+#     m += 1
 
-import turtle
+# import turtle
 
-turtle.color("olive")
-turtle.speed(20)
-turtle.shape("turtle")
+# turtle.color("olive")
+# turtle.speed(20)
+# turtle.shape("turtle")
 
-counter = 0
-while counter < 36:
-    for i in range(4):
-        turtle.forward(100)
-        turtle.right(90)
-    turtle.right(10)
-    counter += 1
+# counter = 0
+# while counter < 36:
+#     for i in range(4):
+#         turtle.forward(100)
+#         turtle.right(90)
+#     turtle.right(10)
+#     counter += 1
 
-turtle.exitonclick();
+# turtle.exitonclick();
 
-import turtle
+# import turtle;
 
-height = 14
-width = 10
+# turtle.shape("turtle");
+# turtle.speed(3);
 
-turtle.speed(25)
-turtle.penup()
+# for i in range(3):
+#     turtle.forward(150);
+#     turtle.left(120);
 
-for y in range(height):
-    for x in range(width):
-        turtle.dot()
-        turtle.forward(20)
-    turtle.backward(20 * width)
-    turtle.right(90)
-    turtle.forward(20)
-    turtle.left(90)
-
-turtle.exitonclick()
+# turtle.exitonclick();
