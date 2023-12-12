8c0781135d8be35b4e56f49b1761a38a10994e31
Al Amin Miah <alaminmiah4274@gmail.com>
2023-12-12 18:27:22 +0600

    drawn a equilateral triangle with function

--- fun.py
+++ fun.py
@@ -26,21 +26,40 @@
 
 
 # to draw a circle using square box:
+# import turtle;
+
+# turtle.speed(35);
+# turtle.color("red");
+# turtle.shape("turtle");
+
+# def draw_square(side_length):
+#     for i in range(4):
+#         turtle.forward(side_length);
+#         turtle.left(90);
+
+# counter = 0;
+# while counter < 36:
+#     draw_square(150);
+#     turtle.right(10);
+#     counter += 1;
+
+# turtle.exitonclick();
+
+# to draw a equilateral triangle: 
 import turtle;
 
-turtle.speed(35);
-turtle.color("red");
-turtle.shape("turtle");
+turtle.shape("turtle")
+turtle.speed(20)
+turtle.color("purple")
 
-def draw_square(side_length):
-    for i in range(4):
-        turtle.forward(side_length);
-        turtle.left(90);
+def equilateral_triangle(side_length):
+    for i in range(3):
+        turtle.forward(side_length)
+        turtle.left(120)
 
-counter = 0;
-while counter < 36:
-    draw_square(150);
-    turtle.right(10);
-    counter += 1;
+counter = 0
+while counter < 1:
+    equilateral_triangle(200)
+    counter += 1
 
-turtle.exitonclick();
+turtle.exitonclick()
