eaed93a03a2c4dcdaccffd9db587ab5c422c85ea
Al Amin Miah <alaminmiah4274@gmail.com>
2023-12-18 16:44:38 +0600

    solved another problem

--- func_exc.py
+++ func_exc.py
@@ -1,25 +1,30 @@
-def avg_numbers(numbers):
-    result = 0
+# def avg_numbers(numbers):
+#     result = 0
     
-    for number in numbers:
-        result += number
+#     for number in numbers:
+#         result += number
     
-    average = result // 2
-    return average
+#     average = result // 2
+#     return average
 
-total_amount = [40, 60, 100, 150, 50, 100]
-print(avg_numbers(total_amount))
+# total_amount = [40, 60, 100, 150, 50, 100]
+# print(avg_numbers(total_amount))
 
-def average_numbers(numbers):
-    result = sum(numbers)
-    average = result // 2
-    return average
+# def average_numbers(numbers):
+#     result = sum(numbers)
+#     average = result // 2
+#     return average
 
-my_money = average_numbers([45, 78, 19, 73, 21])
-print(my_money)
+# my_money = average_numbers([45, 78, 19, 73, 21])
+# print(my_money)
 
-num = int(input("please type an integer number: "))
-def multiply(num):
+# num = int(input("please type an integer number: "))
+# def multiply(num):
+#     for i in range(1, 11):
+#         print(num, "X", i, "=", num*i)
+# multiply(num)
+
+def multiplication(num=1):
     for i in range(1, 11):
         print(num, "X", i, "=", num*i)
-multiply(num)
+multiplication()
