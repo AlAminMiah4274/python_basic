b6df22f57efacc1f53db74ef76df7867f8181815
Al Amin Miah <alaminmiah4274@gmail.com>
2023-12-22 09:41:05 +0600

    len and find method learnt

--- string/string_python.py
+++ string/string_python.py
@@ -8,3 +8,49 @@ s = ''
 # print(s)
 l = len(s)
 # print(s)
+
+# single quote string: ---->
+
+first_s = "Dimik's"
+# print(first_s)
+
+second_s = 'Dimik\'s'
+# print(second_s)
+
+# print(len(first_s))
+
+country = "Australia"
+# print(len(country))
+# print(country[4])
+# print(country[1])
+# print(country[-9])
+
+class_roll = [45, 12, 78, 54, 91, 31]
+# print(class_roll[-5])
+
+# for i in country:
+#     # print(i)
+
+country = "Switzer" + "land"
+# print(country)
+
+number = "50" + "12"
+# print(number)
+
+# find() METHOD: -------->
+
+country = "Switzerland"
+search = country.find("an")
+# print(search)
+
+speech = "It is so cold outside, I am alone, I am alright"
+search = speech.find("alone")
+# print(search)
+
+statement = "You are fucking arrogant asshole. He even did not shoot"
+search = statement.find("not")
+# print(search)
+
+music = "Give me some sunshine. Give me some rain. Give me another chance. I want to grow up again"
+search = music.find("to grow up")
+print(search)
