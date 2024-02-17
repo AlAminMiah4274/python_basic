c56f8becf8b45eccd5cc869089e5f290576a3507
Al Amin Miah <alaminmiah4274@gmail.com>
2024-02-17 23:02:59 +0600

    done excercise 2

---
+++ string/more_string.py
@@ -0,0 +1,6 @@
+# print("this is Al Amin Miah")
+
+x = 2
+y = 2
+result = x * y
+print(result)
--- string/str_excercise.py
+++ string/str_excercise.py
@@ -1,36 +1,51 @@
 # Ex: 1
-def varity_str(string):
-    result = []
-    string_one = ""
-    for capital_letter in string:
-        if capital_letter.isupper():
-            string_one = string_one + capital_letter
-    print(string_one)
-    result.append(string_one)
+# def varity_str(string):
+#     result = []
+#     string_one = ""
+#     for capital_letter in string:
+#         if capital_letter.isupper():
+#             string_one += capital_letter
+#     print(string_one)
+#     result.append(string_one)
 
-    string_two = ""
-    for small_letter in string:
-        if small_letter.islower():
-            string_two = string_two + small_letter
-    print(string_two)
-    result.append(string_two)
+#     string_two = ""
+#     for small_letter in string:
+#         if small_letter.islower():
+#             string_two += small_letter
+#     print(string_two)
+#     result.append(string_two)
 
-    string_three = ""
-    for number in string:
-        if number.isdigit():
-            string_three = string_three + number
-    print(string_three)
-    result.append(string_three)
+#     string_three = ""
+#     for number in string:
+#         if number.isdigit():
+#             string_three += number
+#     print(string_three)
+#     result.append(string_three)
 
-    string_four = ""
-    for others in string:
-        if not others.isalnum() and not others.isspace():
-            string_four = string_four + others
-    print(string_four)
-    result.append(string_four)
+#     string_four = ""
+#     for others in string:
+#         if not others.isalnum() and not others.isspace():
+#             string_four += others
+#     print(string_four)
+#     result.append(string_four)
+
+#     return result
+
+
+# line = input("Please write a sentence. Which contains capital and small letter, number and special characters: ")
+# print(varity_str(line))
+
+# Ex: 2
+
+def position_exchange(string):
+    characters = list(string)
+
+    for i in range(0, len(string)-1, 2):
+        characters[i], characters[i + 1] = characters[i + 1], characters[i]
+
+    result = ''.join(characters)
 
     return result
 
-
-line = "Hello! My Name is Jack. My age is 32. I was born in 1992, *&"
-print(varity_str(line))
+function_parameter = input("Write a word: ")
+print(position_exchange(function_parameter))
